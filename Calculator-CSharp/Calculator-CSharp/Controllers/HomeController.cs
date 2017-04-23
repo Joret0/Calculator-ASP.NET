76da64113a528b7247d4cfa00d624ad813413bf0
Georgi Stalev <georgi_stalev@abv.bg>
2017-04-23 21:42:19 +0300

    upload Calculator C#

---
+++ Calculator-CSharp/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Mvc;
+using Calculator_CSharp.Models;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Models.Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Models.Calculator calculator)
+        {
+            calculator.Result = CalculatorResult(calculator);
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculatorResult(Calculator calculator)
+        {
+            var result = 0m;
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+            }
+            return result;
+        }
+    }
+}
